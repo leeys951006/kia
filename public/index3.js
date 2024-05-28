cf149364e6517050d74cfcdf9e20d1b9d568f717
이연승 <leeys951006@gmail.com>
2024-05-28 14:42:18 +0900

    kia 파일 추가

---
+++ public/index3.js
@@ -0,0 +1,28 @@
+const button1 = document.getElementById('button1')
+const button2 = document.getElementById('button2')
+const button3 = document.getElementById('button3')
+const button4 = document.getElementById('button4')
+const button5 = document.getElementById('button5')
+const button6 = document.getElementById('button6')
+
+
+button1.addEventListener('click', function(){
+  alert("주문이 완료되었습니다.")
+})
+button2.addEventListener('click', function(){
+  alert("주문이 완료되었습니다.")
+})
+button3.addEventListener('click', function(){
+  alert("주문이 완료되었습니다.")
+})
+button4.addEventListener('click', function(){
+  alert("주문이 완료되었습니다.")
+})
+button5.addEventListener('click', function(){
+  alert("주문이 완료되었습니다.")
+})
+button6.addEventListener('click', function(){
+  alert("주문이 완료되었습니다.")
+})
+
+
